f09890034fa845bd48c33af4c49740bb820158d8
jaaf <jose.fournier@mailbox.org>
2023-08-28 05:01:34 +0200

    add filtering to recipe list

--- RecipeListWidget.py
+++ RecipeListWidget.py
@@ -10,7 +10,7 @@ You should have received a copy of the GNU General Public License along with thi
 
 from database.recipes.recipe import all_recipe
 from ListModels import RecipeListModel
-from PyQt6.QtWidgets import QListView,QVBoxLayout,QHBoxLayout,QPushButton,QSpacerItem,QWidget,QLabel,QComboBox
+from PyQt6.QtWidgets import QListView,QVBoxLayout,QHBoxLayout,QPushButton,QSpacerItem,QWidget,QLabel,QComboBox,QCheckBox,QLineEdit,QFrame
 from PyQt6 import QtCore,QtWidgets
 from PyQt6.QtCore import QSize,Qt
 from PyQt6.QtGui import QIcon,QPalette,QFont
@@ -53,6 +53,50 @@ class RecipeListWidget(QWidget):
         self.sortButton.setIconSize(self.icon_size)
         self.sortButton.setMaximumSize(40,40)
         toolbarLayout=QHBoxLayout()
+
+        #FILTERS
+        self.filterLayout=QHBoxLayout()
+
+        self.typeFilterLayout=QVBoxLayout()
+        self.typeFilter=QCheckBox("Filtrer type")
+        self.typeFilterEdit=QLineEdit()
+        self.typeFilterLayout.addWidget(self.typeFilter)
+        self.typeFilterLayout.addWidget(self.typeFilterEdit)
+
+
+        self.nameFilterLayout=QVBoxLayout()
+        self.nameFilter=QCheckBox("Filtrer noms")
+        self.nameFilterEdit=QLineEdit()
+        self.nameFilterLayout.addWidget(self.nameFilter)
+        self.nameFilterLayout.addWidget(self.nameFilterEdit)
+
+        self.styleFilterLayout=QVBoxLayout()
+        self.styleFilter=QCheckBox("Filtrer styles")
+        self.styleFilterEdit=QLineEdit()
+        self.styleFilterLayout.addWidget(self.styleFilter)
+        self.styleFilterLayout.addWidget(self.styleFilterEdit)
+
+        self.nameFilterLayout.setSpacing(0)
+        self.styleFilterLayout.setSpacing(0)
+        self.typeFilterLayout.setSpacing(0)
+        self.filterButton=QPushButton()
+        self.filterButton.setIcon(QIcon(self.icon_path+'filter-slash-svgrepo-com.svg'))
+        self.filterButton.setIconSize(self.icon_size)
+        self.filterButton.setToolTip("Appliquer les filtres")
+        self.filterLayout.addWidget(self.filterButton)
+        self.filterLayout.addLayout(self.nameFilterLayout)
+        self.filterLayout.addLayout(self.styleFilterLayout)
+        self.filterLayout.addLayout(self.typeFilterLayout)
+        self.filterHelpButton=QPushButton("?")
+        self.filterHelpButton.resize(24,24)
+        self.filterHelpButton.setMaximumWidth(24)
+        self.filterHelpButton.setToolTip("Obtenir de l'aide sur le filtrage")
+        self.filterHelpButton.setStyleSheet("background-color:green;color:white")
+        self.filterLayout.addWidget(self.filterHelpButton)
+
+        self.hide_sub_filter("name")
+        self.hide_sub_filter("style")
+        self.hide_sub_filter("type")
         self.newButton.setIcon(QIcon(self.icon_path+'add-square-svgrepo-com.svg'))
         self.newButton.setIconSize(self.icon_size)
         self.newButton.setMaximumSize(40,40)
@@ -64,6 +108,11 @@ class RecipeListWidget(QWidget):
         spacerItem = QtWidgets.QSpacerItem(40, 10, QtWidgets.QSizePolicy.Policy.Expanding, QtWidgets.QSizePolicy.Policy.Fixed)
         spacerItemSmall = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Policy.Fixed, QtWidgets.QSizePolicy.Policy.Fixed)
         toolbarLayout.addItem(spacerItem)
+
+        self.filterFrame=QFrame()
+        self.filterFrame.setLayout(self.filterLayout)
+        self.filterFrame.setStyleSheet("background-color:honeydew;")
+        toolbarLayout.addWidget(self.filterFrame)
         toolbarLayout.addWidget(self.sortButton)
         toolbarLayout.addWidget(self.sortCombo)
         toolbarLayout.addWidget(self.newButton)
@@ -101,7 +150,73 @@ class RecipeListWidget(QWidget):
         self.newButton.clicked.connect(self.new_recipe)
         self.listView.clicked.connect(self.select_recipe)
         self.sortCombo.currentTextChanged.connect(lambda: self.sort_list(self.sortCombo.currentText()))
-        
+        self.nameFilter.stateChanged.connect(self.name_filter_changed)
+        self.styleFilter.stateChanged.connect(self.style_filter_changed)
+        self.typeFilter.stateChanged.connect(self.type_filter_changed)
+        self.filterButton.clicked.connect(self.apply_filters)
+        self.filterHelpButton.clicked.connect(self.show_contextual_help)
+
+    def show_contextual_help(self):
+        helpPopup=HelpMessage() 
+        filename="help/Head.html"
+        prepend=open(filename,'r',encoding="utf-8").read()
+        helpPopup.set_title('À propos du filtrage ')
+        filename="help/BrewListFilterHelp.html"
+        text=open(filename,'r',encoding="utf-8").read()
+        helpPopup.set_message(prepend+text)
+        helpPopup.exec()
+
+    def apply_filters(self):
+        items=self.recipes
+        filtered=list(filter(lambda x: \
+                                (self.nameFilterEdit.text().upper() in x.name.upper() or not self.nameFilter.isChecked()) and \
+                                (self.typeFilterEdit.text().upper() in x.rtype.upper( ) or not self.typeFilter.isChecked()) and \
+                                    (self.styleFilterEdit.text().upper() in x.style.upper() or not self.styleFilter.isChecked()),items))
+        self.model.recipes=filtered
+        self.sort_list(self.sortCombo.currentText())
+
+    def type_filter_changed(self):
+        if self.typeFilter.isChecked():
+            self.show_sub_filter("type")
+        else:
+            self.hide_sub_filter("type")
+            
+    def style_filter_changed(self):
+        print("in style_filter_changed")
+        if self.styleFilter.isChecked():
+            self.show_sub_filter("style")
+        else:
+            self.hide_sub_filter("style")  
+
+    def name_filter_changed(self):
+        if self.nameFilter.isChecked():
+            self.show_sub_filter("name")
+        else:
+            self.hide_sub_filter("name")        
+
+    def hide_sub_filter(self,mode):
+        match mode:
+            case "type":
+                self.typeFilterEdit.setVisible(False)
+                self.typeFilterEdit.setText("")
+
+            case "name":
+                self.nameFilterEdit.setVisible(False)
+                self.nameFilterEdit.setText("")
+            case "style":
+                self.styleFilterEdit.setVisible(False)    
+                self.styleFilterEdit.setText("")    
+
+    def show_sub_filter(self,mode):
+        print('show again')
+        match mode:
+            case "type":
+                self.typeFilterEdit.setVisible(True)
+            case "name":
+                self.nameFilterEdit.setVisible(True)
+            case "style":
+                print("style")
+                self.styleFilterEdit.setVisible(True)   
 
     def sort_list(self, mode):
         #self.source_list.sort(key=lambda x: (x.brand,x.name,x.version))
